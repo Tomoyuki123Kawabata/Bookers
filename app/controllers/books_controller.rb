17d53df9924b78858f7b49274da7775cf9de237a
Tomoyuki123Kawabata <122082755+Tomoyuki123Kawabata@users.noreply.github.com>
2023-02-06 08:22:38 +0000

    [ADD]フラッシュメッセージ、バリデーションを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,18 @@ class BooksController < ApplicationController
     
     @book = Book.new
   end
+  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Registration successfully"
+      redirect_to book_path(@book.id)
+    else
+     @books = Book.all
+     render :index
+    end
   end
+  
   def show
     @book = Book.find(params[:id])
   end
@@ -16,14 +23,23 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    
+    if @book.update(book_params)
+     flash[:notice] = "Update successfully"
+     redirect_to book_path(@book.id)
+    else
+     flash.now[:notice] = "Update error"
+     render :edit
+    end
   end
+  
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Delete successfully"
     redirect_to '/books'
   end
   
